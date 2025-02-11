41e0bb2432e0a12e38b1056ff4ab7839d02125e6
Akash <akash178kusha@gmail.com>
2025-02-11 20:06:49 +0530

    Initial commit

---
+++ day1.java
@@ -0,0 +1,9 @@
+public class day1{
+    public static void main(String[] args) {
+        int num1 =20;
+        int num2 = 10;
+        int sum = num1+num2;
+        System.out.println("The sum of " +num1+ "and" +num2+ "is:" +sum);
+
+    }
+}
