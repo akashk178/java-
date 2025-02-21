63dc4f2f39a7caffbdc3fa4d8ef0e5dd877f0619
Akash <akash178kusha@gmail.com>
2025-02-21 20:43:33 +0530

    your commit message

--- day1.java
+++ day1.java
@@ -1,9 +1,29 @@
+// public class day1{
+//     public static void main(String[] args) {
+//         int num1 =20;
+//         int num2 = 10;
+//         int sum = num1+num2;
+//         System.out.println("The sum of " +num1+ "and" +num2+ "is:" +sum);
+
+//     }
+// }
+// public class day1 {
+//     public static void main(String[] args)
+//     {
+//         int num1 = 5;
+//         int num2 = 6;
+//         int result = num1 + num2;
+//         System.out.println(result);
+//     }
+// }
+
+
 public class day1{
-    public static void main(String[] args) {
-        int num1 =20;
-        int num2 = 10;
-        int sum = num1+num2;
-        System.out.println("The sum of " +num1+ "and" +num2+ "is:" +sum);
+    public static void main(String a[]){
+        int num1 =24;
+        float f = 6.52f;
+        float res = num1 + f;
+        System.out.println(res);
 
     }
 }
